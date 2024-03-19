da0988107a193c5dc669cd0bd4946c61255c1536
tsxv478 <atci39@rambler.ru>
2024-03-19 23:50:44 +0300

    Top bar default

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static const unsigned int gappov    = 30;       /* vert outer gap between window
 static int enablegaps               = 1;        /* 1 means enable gaps on startup */
 static int smartgaps                = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const int barheight          = 2;        /* 2 is the default spacing around the bar's font */
 static const char *fonts[]          = { "monospace:size=11" };
 static const char dmenufont[]       = "monospace:size=11";
