811731b889c4e4945d9156779017c1ddfab8cd72
tsxv478 <atvx717@infraredcombat.xyz>
2024-10-23 17:24:43 +0300

    Brighter cyan color

--- config.def.h
+++ config.def.h
@@ -19,7 +19,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#008888";
+static const char col_cyan[]        = "#55aaaa";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
